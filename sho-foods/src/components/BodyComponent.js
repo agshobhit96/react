aada586eab7589b802b865d3c674601c3ac6e514
shobhit agrawal <shobhit.agrawal@efi.com>
2024-05-03 01:16:47 +0530

    Filtering the data and using State variables

--- sho-foods/src/components/BodyComponent.js
+++ sho-foods/src/components/BodyComponent.js
@@ -1,10 +1,13 @@
 import ResCards from "./ResCards";
 import resItems from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ShimmerCards from "./ShimmerCards";
 
 
 const BodyComponent = () => {
-    const [listOfRes, setListOfRes] = useState(resItems);
+    const [listOfRes, setListOfRes] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    const [filteredListOfRes, setFilteredListOfRes] = useState([]);
 
 
     const filterBtnClicked = () => {
@@ -12,15 +15,40 @@ const BodyComponent = () => {
         let updatedList = listOfRes.filter((item) => {
             return item.data.rating >= 4;
         });
-        setListOfRes(updatedList);
+        setFilteredListOfRes(updatedList);
     };
+
+    // gets called after the first render
+    useEffect(() => {
+        // API call
+        // Fetch data
+        // Update state
+        fetchData();
+    },[]);
+
+    fetchData = () => {
+        // MAKE API CALL    
+        setListOfRes(resItems);
+        setFilteredListOfRes(resItems);
+    }
+
+    if (listOfRes.length === 0) {
+        // Show loader or Shimmer UI
+        return <ShimmerCards />;
+    }
 
     return (
         <div className="body">
             <div className="filter-container">
                 <div className="search-container">
-                    <input type="text" placeholder="Search" />
-                    <button>Search</button>
+                    <input type="text" placeholder="Search" value={searchText} onChange={(e)=> setSearchText(e.target.value)}/>
+                    <button onClick={()=> {
+                        console.log("Search Button Clicked", searchText);
+                        let filteredList = listOfRes.filter((item) => {
+                            return item.data.name.toLowerCase().includes(searchText.toLowerCase());
+                        });
+                        setFilteredListOfRes(filteredList);
+                    }}>Search</button>
                 </div>
                 <button className="filterBtn" onClick={()=> {
                     filterBtnClicked();
@@ -29,7 +57,7 @@ const BodyComponent = () => {
             </div>
             <div className="cards-container">
                 {
-                    listOfRes.map((item, index) => {
+                    filteredListOfRes.map((item, index) => {
                         return (
                             <ResCards key={index} resList={item}/>
                         );
--- sho-foods/src/components/HeaderComponent.js
+++ sho-foods/src/components/HeaderComponent.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const HeaderComponent = () => {
+    const [btnName, setBtnName] = useState("Sign In");
     return (
         <div className="header">
             <div className="logo-container">
@@ -12,6 +14,9 @@ const HeaderComponent = () => {
                     <li>About</li>
                     <li>Contact</li>
                     <li>Cart</li>
+                    <button className="loginBtn" onClick={() => {
+                        btnName === "Sign In" ? setBtnName("Sign Out") : setBtnName("Sign In");
+                    }}>{btnName}</button>
                 </ul>
             </div>
         </div>
